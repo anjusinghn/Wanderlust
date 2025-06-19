6178306975d57262e2f728fd5f5a7754d5f08eec
Anju Singh <anjusinghn123@gmail.com>
2025-06-19 22:36:57 +0530

    Uploading image on cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -13,14 +13,12 @@ const upload = multer({ storage })
 router
 .route("/")
 .get(wrapAsync(listingController.index))
-// .post(
-//   isLoggedIn,
-//   validateListing,
-//   wrapAsync(listingController.createListing)
-// )
-.post(upload.single('listing[image]'), (req,res) => {
-  res.send(req.file);
-});
+.post(
+  isLoggedIn,
+  upload.single('listing[image]'),
+  validateListing,
+  wrapAsync(listingController.createListing)
+);
 
 //New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
