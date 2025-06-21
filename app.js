565ed0916adfd75306cbb1ef7356bb552db0cd8d
Anju Singh <anjusinghn123@gmail.com>
2025-06-22 00:38:15 +0530

    Updated UI

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("YAY Im working");
-});
+// app.get("/", (req, res) => {
+//   res.send("YAY Im working");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
--- public/js/map.js
+++ public/js/map.js
@@ -7,7 +7,7 @@ const map = new mapboxgl.Map({
   zoom: 7, // starting zoom
 });
 
-const marke2 = new mapboxgl.Marker({ color: 'red'})
+const marke = new mapboxgl.Marker({ color: 'red'})
   .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
   .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.location}</h4><p>Exact location Provided after booking</p>`))
   .addTo(map);
